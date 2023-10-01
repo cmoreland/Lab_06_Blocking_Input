2f3e57beecf986eecd6491e59f619ce374640ce2
Cameron <morelacn@mail.uc.edu>
2023-09-30 22:51:51 -0400

    Finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Give a temperature in Celsius: ");
+        boolean done = false;
+        do {
+            if (in.hasNextInt()){
+                int temp = in.nextInt();
+                int far = (temp * (9/5)) + 32;
+                System.out.println(temp + " degrees Celsius in Fahrenheit is " + far + " degrees.");
+                done = true;
+            }
+            else {
+                System.out.println("That's not quite right. Try another input.");
+            }
+
+        }while (!done);
     }
 }
